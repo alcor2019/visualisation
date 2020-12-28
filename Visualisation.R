b286f3278f287a0fca7a0c3d788fc524d7628612
Alain CORVISIER <alain.corvisier@gmail.com>
2020-12-28 14:44:23 +0100

    commit pour test push depuis RStudio

--- Visualisation.R
+++ Visualisation.R
@@ -9,4 +9,16 @@ diamonds2 %>% ggplot() + aes(x=price) + geom_histogram(bins = 20, aes(fill = cut
 
 diamonds2 %>% ggplot() + aes(x=carat, y=price, color=cut) + geom_point()
 
+D <- data.frame(X=seq(-2*pi,2*pi,by=0.01))
+ggplot(D) + aes(x=X,y=sin(X)) + geom_line()
 
+
+ggplot(diamonds2) + aes(x=price) + geom_histogram(bins=40, col="black", fill="white")
+
+ggplot(diamonds2) + aes(x=price, y=..count..) + geom_histogram(bins=40, col="black", fill="white")
+
+ggplot(diamonds2) + aes(x=price, y=..density..) + geom_histogram(bins=40, col="black", fill="white")
+
+ggplot(diamonds2) + aes(x=price) + stat_bin(bins=40, col="black", fill="white")
+
+ggplot(diamonds2) + aes(x=price, y=..density..) + stat_bin(bins=40, col="black", fill="white")
